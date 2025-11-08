827c427f082cc2cab657b528f33e53b019f4ad8f
BordiyanNick <162913412+BordiyanNick@users.noreply.github.com>
2025-11-08 19:09:51 +0200

    Update ProgramCode.py

--- .github/workflows/ProgramCode.py
+++ .github/workflows/ProgramCode.py
@@ -1,7 +1,6 @@
-# ProgramCode.py
-# Виведення назви програмного продукту
-def main():
-    print("Health Monitoring System - Laboratory Work #4")
-
-if __name__ == "__main__":
-    main()
+# ProgramCodeModified.py
+# Приклад стилістично неправильного коду (для перевірки лінтером)
+def main( ):
+ print(  "Health Monitoring System - Laboratory Work #4" )
+if __name__=="__main__":
+ main()
