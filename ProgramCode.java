6a386d997459efafc3e709e1aee61f9f9c500756
BordiyanNick <162913412+BordiyanNick@users.noreply.github.com>
2025-11-09 12:38:00 +0200

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,23 +1,6 @@
 package com.example;
 
-/**
- * Головний клас програми, що виводить назву продукту.
- */
-public final class ProgramCode {
-
-    /**
-     * Приватний конструктор.
-     */
-    private ProgramCode() {
-        // Утилітарний клас
-    }
-
-    /**
-     * Головний метод програми.
-     *
-     * @param args Аргументи командного рядка.
-     */
-    public static void main(final String[] args) {
-        System.out.println("Health Monitoring System - Laboratory Work #4");
-    }
-}
+public class ProgramCode {
+public static void main(String[] args){
+System.out.println("Health Monitoring System-Lab4");
+}}
