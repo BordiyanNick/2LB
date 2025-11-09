4d16e8dbd5fa8e0b7f5ce3a18c8b080b00154e83
BordiyanNick <162913412+BordiyanNick@users.noreply.github.com>
2025-11-09 12:27:16 +0200

    Create ProgramCode.java

---
+++ ProgramCode.java
@@ -0,0 +1,23 @@
+package com.example;
+
+/**
+ * Головний клас програми, що виводить назву продукту.
+ */
+public final class ProgramCode {
+
+    /**
+     * Приватний конструктор.
+     */
+    private ProgramCode() {
+        // Утилітарний клас
+    }
+
+    /**
+     * Головний метод програми.
+     *
+     * @param args Аргументи командного рядка.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Health Monitoring System - Laboratory Work #4");
+    }
+}
